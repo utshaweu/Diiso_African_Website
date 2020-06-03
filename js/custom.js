d9c7da90605985892f6ea5a9b175d07332b6e371
utshaweu <mahmudsajib95@gmail.com>
2020-06-03 23:54:37 +0600

    team part added

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,26 @@
 $(function(){
   'use strict'
+  //Back to top js
+  var back2top = $(".back-to-top");
+  var html_body = $('html,body');
+  back2top.click(function(){
+    html_body.animate({scrollTop:0},1000);
+  });
 
-  //Preloader js
-  $(window).on('load',function(){
-    $(".preloader").delay(1000).fadeOut(1000);
+  
+  //Smooth Scroll js
+  var html_body = $('html, body');
+  $('a').on('click', function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+              html_body.animate({
+                  scrollTop: target.offset().top - 45
+              }, 1500);
+              return false;
+          }
+      }
   });
 
   //Sticky Menu js
@@ -23,33 +40,16 @@ $(function(){
          back2top.fadeOut(500);
      }
  });
+ //Preloader js
+ $(window).on('load',function(){
+  $(".preloader").delay(1000).fadeOut(1000);
+  });
 
  // Closes responsive menu when a scroll link is clicked
  $('.nav-link').on('click', function () {
   $('.navbar-collapse').collapse('hide');
   });
 
-  //Back to top js
-  var back2top = $(".back-to-top");
-  var html_body = $('html,body');
-  back2top.click(function(){
-    html_body.animate({scrollTop:0},1000);
-  });
-
-  //Smooth Scroll js
-  var html_body = $('html, body');
-  $('a').on('click', function () {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-          if (target.length) {
-              html_body.animate({
-                  scrollTop: target.offset().top - 45
-              }, 1500);
-              return false;
-          }
-      }
-  });
 
 
   //Counter Up Js
@@ -57,6 +57,7 @@ $(function(){
     delay: 10,
     time: 2000,
   });
+
 
   //Wow Js
   new WOW().init();
